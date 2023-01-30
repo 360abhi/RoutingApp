b8e3230e2e95b8f0bbcf7d1f80b52ac5203127f0
Abhishek Chhawari <abhishekch360@gmail.com>
2023-01-31 00:56:49 +0530

    contact data passing and scrolling

--- src/app/Services/contact.service.ts
+++ src/app/Services/contact.service.ts
@@ -7,22 +7,30 @@ export class ContactService {
             {
                 'name' : 'Ankit',
                 'role' : 'React Developer',
-                'imageUrl' : '../../assets/ankit.jpg'
+                'imageUrl' : '../../assets/ankit.jpg',
+                'email': 'ankit@developersbay.co.in',
+                'phone' : '1234567890'
             },
             {
                 'name' : 'Abhishek',
                 'role' : 'Angular Developer',
-                'imageUrl' : '../../assets/abhishek.jpg'
+                'imageUrl' : '../../assets/abhishek.jpg',
+                'email': 'abhishek@developersbay.co.in',
+                'phone' : '3237890'
             },
             {
                 'name' : 'Dhiraj',
                 'role' : 'C++ Developer',
-                'imageUrl' : '../../assets/dhiraj.jpg'
+                'imageUrl' : '../../assets/dhiraj.jpg',
+                'email': 'dhiraj@developersbay.co.in',
+                'phone' : '5674567890'
             },
             {
                 'name' : 'Ritik',
                 'role' : 'Database Engineer',
-                'imageUrl' : '../../assets/ritik.jpeg'
+                'imageUrl' : '../../assets/ritik.jpeg',
+                'email': 'ritik@developersbay.co.in',
+                'phone' : '876567890'
             }
         ]
 
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
 import { ContactService } from '../Services/contact.service';
+import {ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -10,8 +11,35 @@ import { ContactService } from '../Services/contact.service';
 export class ContactComponent {
 
     contacts;
+    isSelected : boolean= false;
+    email: String = "";
+    name: String  = "";
+    role: String = "";
+    phone: String = "";
+    successmsg : boolean = false;
+    target :any;
 
     constructor(public contactservice: ContactService){
       this.contacts = contactservice.getDetails();
     }
+
+   
+    btnClicked(name:any, role:any, email:any, phone:any) : void {
+      this.isSelected = true;
+      this.name = name;
+      this.role = role;
+      this.phone = phone;
+      this.email = email;
+      this.successmsg = false;
+      document.getElementById('target')?.scrollIntoView({behavior : "smooth"})
+    }
+
+    sendMessage() : void {
+      this.isSelected = false;
+      this.successmsg = true;
+    }
+
+    // my(target:HTMLElement) : void{
+    //   target.scrollIntoView({behavior:'smooth'});
+    // }
 }
