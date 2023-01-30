2fe3e182811be93036d96febc4d29f3ced7a406c
Abhishek Chhawari <abhishekch360@gmail.com>
2023-01-31 02:35:16 +0530

    home component addition and 404 error page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,29 @@ import { ContactService } from './Services/contact.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { Route,RouterModule,Routes } from '@angular/router';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes : Routes = [
-  {
-    path : 'home', component : HomeComponent
-  },
-  {
-    path:'about', component : AboutComponent
-  },
-  {
-    path: 'contact', component : ContactComponent
-  }
+  {path : '', component:HomeComponent},
+  // { path : 'navbar', component: NavBarComponent},
+  { path: 'home' , component : HomeComponent},
+  { path:'about', component : AboutComponent },
+  { path: 'contact', component : ContactComponent },
+  { path:'**', component : PagenotfoundComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    HomeComponent,
-    AboutComponent
+    NavBarComponent,
+    AboutComponent,
+    PagenotfoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
